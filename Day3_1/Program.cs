23a959586e9a27de725113a94a8604d4e85dd47a
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-12-03 17:52:31 +0100

    - Day 3

---
+++ Day3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+
+var matches = new List<char>();
+foreach (var line in input)
+{
+	var lineHalf = line.Length / 2;
+
+	var firstCompartment = new HashSet<char>(line[..lineHalf]);
+	var secondCompartment = new HashSet<char>(line[lineHalf..]);
+
+	matches.Add(firstCompartment.Intersect(secondCompartment).First());
+}
+
+static int GetPriority(char item)
+{
+	return item switch
+	{
+		>= 'A' and <= 'Z' => item - 'A' + 27,
+		_ => item - 'a' + 1,
+	};
+}
+
+Console.WriteLine(matches.Sum(GetPriority));
+Console.ReadLine();
---
+++ Day3_2/Program.cs
@@ -0,0 +1,28 @@
+﻿var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+
+var chunks = input.Chunk(3);
+
+var matches = chunks.Select(chunk =>
+{
+	var hashSets = chunk.Select(x => x.ToHashSet()).ToList();
+
+	return hashSets
+		.Skip(1)
+		.Aggregate(hashSets.First(), (current, prev) =>
+		{
+			current.IntersectWith(prev);
+			return current;
+		})
+		.First();
+});
+static int GetPriority(char item)
+{
+	return item switch
+	{
+		>= 'A' and <= 'Z' => item - 'A' + 27,
+		_ => item - 'a' + 1,
+	};
+}
+
+Console.WriteLine(matches.Sum(GetPriority));
+Console.ReadLine();
