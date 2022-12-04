3feff6b714fd892c647fe7c49c6b5460b443d3bd
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-12-04 11:39:14 +0100

    - Day 4!

---
+++ Day4_1/Program.cs
@@ -0,0 +1,34 @@
+﻿var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+
+var fullMatchCounter = 0;
+foreach (var line in input)
+{
+	var span = line.AsSpan();
+	var separatorIndex = span.IndexOf(',');
+
+	var firstRange = ParseRange(span[..separatorIndex]);
+	var secondRange = ParseRange(span[(separatorIndex + 1)..]);
+
+	if (RangeIntersects(firstRange, secondRange) || RangeIntersects(secondRange, firstRange))
+	{
+		fullMatchCounter++;
+	}
+}
+
+static Range ParseRange(ReadOnlySpan<char> input)
+{
+	var dashIndex = input.IndexOf('-');
+
+	var start = int.Parse(input[..dashIndex]);
+	var end = int.Parse(input[(dashIndex + 1)..]);
+
+	return start..end;
+}
+
+static bool RangeIntersects(Range range1, Range range2)
+{
+	return range1.Start.Value >= range2.Start.Value && range1.End.Value <= range2.End.Value;
+}
+
+Console.WriteLine(fullMatchCounter);
+Console.ReadLine();
---
+++ Day4_2/Program.cs
@@ -0,0 +1,35 @@
+﻿var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt"));
+
+var fullMatchCounter = 0;
+foreach (var line in input)
+{
+	var span = line.AsSpan();
+	var separatorIndex = span.IndexOf(',');
+
+	var firstRange = ParseRange(span[..separatorIndex]);
+	var secondRange = ParseRange(span[(separatorIndex + 1)..]);
+
+	if (RangeIntersects(firstRange, secondRange) || RangeIntersects(secondRange, firstRange))
+	{
+		fullMatchCounter++;
+	}
+}
+
+static Range ParseRange(ReadOnlySpan<char> input)
+{
+	var dashIndex = input.IndexOf('-');
+
+	var start = int.Parse(input[..dashIndex]);
+	var end = int.Parse(input[(dashIndex + 1)..]);
+
+	return start..end;
+}
+
+static bool RangeIntersects(Range range1, Range range2)
+{
+	static bool Between(Index index, Range range) => index.Value >= range.Start.Value && index.Value <= range.End.Value;
+	return Between(range1.Start, range2) || Between(range1.End, range2);
+}
+
+Console.WriteLine(fullMatchCounter);
+Console.ReadLine();
