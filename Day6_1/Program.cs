85a8754d77509837477a450d3144181920e2103e
Stefan Schranz <stefan.daniel.schranz96@gmail.com>
2022-12-07 10:14:44 +0100

    - Day 6 done

--- Day6_1/Program.cs
+++ Day6_1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt")).First();
+
+var inputStack = new Queue<char>(input[..4]);
+for (var i = 4; i < input.Length; ++i)
+{
+	if (new HashSet<char>(inputStack).Count == 4)
+	{
+		Console.WriteLine(i);
+		break;
+	}
+
+	inputStack.Dequeue();
+	inputStack.Enqueue(input[i]);
+}
+
+Console.ReadLine();
--- Day6_2/Program.cs
+++ Day6_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var input = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "input.txt")).First();
+
+const int markerPosition = 14;
+var inputStack = new Queue<char>(input[..markerPosition]);
+for (var i = markerPosition; i < input.Length; ++i)
+{
+	if (new HashSet<char>(inputStack).Count == markerPosition)
+	{
+		Console.WriteLine(i);
+		break;
+	}
+
+	inputStack.Dequeue();
+	inputStack.Enqueue(input[i]);
+}
+
+Console.ReadLine();
